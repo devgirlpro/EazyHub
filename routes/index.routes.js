e418303c54b48c70736a1159a90b56bd3a7daebe
devgirl1992 <azadeh.galidari.1992@gmail.com>
2022-07-25 19:13:13 +0200

    employees page - employee delete - employee details page- navbar- welcome page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,54 @@
 const router = require("express").Router();
+const { response } = require("express");
+const User = require("../models/User");
+const Vehicle = require("../models/Vehicle");
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get("/employees/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => next(err));
 });
+
+router.get("/employees", (req, res) => {
+  User.find().then((response) => {
+    console.log("server response=>", response);
+    res.json(response);
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  Project.findById(req.params.id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((err) => next(err));
+});
+
+/* GET employee page */
+//access to the user from db
+// router.get("/employee", (req, res, next) => {
+//   User.find()
+//    .populate("vehicle")
+//    .then(userData => {
+//       res.render("employee", {userData})
+//    })
+//    .catch(error => console.log(error))
+// });
+
+// router.post('/employee', (req, res, next) => {
+//   const {username, password, name, lastName, street, email, phone} = req.body
+//   // validation
+//   if (password.length < 4) {
+//     res.render('employee', { message: 'Password has to be 4 chars min' })
+//     return
+//   }
+//   // check if username is not empty
+//   if (username === '') {
+//     res.render('signup',{layout: false} , { message: 'Username cannot be empty' })
+//     return
+//   }
 
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
